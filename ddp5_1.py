deeff2f42f3591173792c3ec47039c395d13e3c0
marissa azzahra <marisaazzahra01@gmail.com>
2024-11-05 09:59:20 +0700

    pertemuan-5
    ddp5_marissa azzahra_0110124033_si10

---
+++ ddp5_1.py
@@ -0,0 +1,6 @@
+kendaraan = ["hrv", "mobil", "1498cc", "putih", "roda 4"]
+kendaraan.append("570.000.000")
+kendaraan.append("matic")
+print(kendaraan)
+kendaraan.insert(2, "honda")
+print(kendaraan)
