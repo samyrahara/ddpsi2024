a5c1bd13f5ab4a36f7c978e786a636f2ad3e26b8
marissa azzahra <marisaazzahra01@gmail.com>
2024-11-20 15:46:50 +0700

    pertemuan 9

---
+++ DDP1.py
@@ -0,0 +1,8 @@
+print('----mencari celcius ke fahrenhet ----')
+def celcius_ke_fahrenheit(celcius):
+    fahrenheit = (celcius * 9/5) + 32
+    return fahrenheit
+
+print(celcius_ke_fahrenheit(0))
+print(celcius_ke_fahrenheit(100))
+
